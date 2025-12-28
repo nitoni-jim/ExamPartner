1dbe97b343bdeda0540179819e0fc69ef12acfef
nitoni-jim <nitoni4fj@gmail.com>
2025-12-28 17:55:09 +0100

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,10 +1,10 @@
 
-const CACHE_NAME = "ExamPartner v5";
+const CACHE_NAME = "ExamPartner v6";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=5",
+  "./app.js?v=6",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
